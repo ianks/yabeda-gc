c7d6955324c982065827a69b7457e15e0fabf730
Andrey Novikov <envek@envek.name>
2024-01-04 17:42:36 +0900

    Add spec for new metrics

--- lib/yabeda/gc.rb
+++ lib/yabeda/gc.rb
@@ -54,7 +54,7 @@ module Yabeda
       gauge :time, tags: [], comment: "The total time spent in garbage collections" if RUBY_VERSION >= "3.1"
 
       if RUBY_VERSION >= "3.3"
-        gauge :marking_time, tags: [], comment: "Time spent in the marking phase" 
+        gauge :marking_time, tags: [], comment: "Time spent in the marking phase"
         gauge :sweeping_time, tags: [], comment: "Time spent in the sweeping phase"
       end
 
