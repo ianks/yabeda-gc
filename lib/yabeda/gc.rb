b7f5fd7e45ee915f12018c49f11e10c5f1c33393
Jeremy Daer <jeremydaer@gmail.com>
2024-10-31 18:39:17 -0700

    Keep gauges in sync with GC.stat (#6)
    Omit defunct stats and support new ones automatically.
    
    Stats without a verbose comment default to their own name since the Prometheus exporter requires one.

--- lib/yabeda/gc.rb
+++ lib/yabeda/gc.rb
@@ -8,54 +8,51 @@ module Yabeda
   module GC
     EMPTY_HASH = {}.freeze
 
+    # Don't use a constant. No need to retain this after registering gauges.
+    comments = {
+      count: "Count of all GCs",
+      compact_count: "Count of all GC compactions",
+      minor_gc_count: "Count of minor GCs",
+      major_gc_count: "Count of major GCs",
+      heap_allocated_pages: "Total number of pages allocated for the heap",
+      heap_sorted_length: "Length of the sorted heap",
+      heap_allocatable_pages: "Total number of allocatable heap pages",
+      heap_available_slots: "Total number of slots in heap pages",
+      heap_live_slots: "Number of live objects slots",
+      heap_free_slots: "Number of free object slots",
+      heap_final_slots: "Number of object slots with finalizers attached to them",
+      heap_marked_slots: "Count of old objects which survived more than 3 GC cycles and number of write-barrier unprotected objects",
+      heap_eden_pages: "Number of pages allocated for the eden heap",
+      heap_tomb_pages: "Number of pages allocated for the tomb heap",
+      total_allocated_pages: "Total number of allocated pages over the lifetime of this process",
+      total_freed_pages: "Total number of freed pages over the lifetime of this process",
+      total_allocated_objects: "Total number of allocated objects over the lifetime of this process",
+      total_freed_objects: "Total number of freed objects over the lifetime of this process",
+      malloc_increase_bytes: "Total bytes allocated to objects",
+      malloc_increase_bytes_limit: "Bytes limit that will trigger garbage collection of objects",
+      remembered_wb_unprotected_objects: "Number of write-barrier unprotected objects in the remembered set",
+      remembered_wb_unprotected_objects_limit: "Limit on write-barrier unprotected objects allowed in the remembered set",
+      old_objects: "Number of old objects",
+      old_objects_limit: "Limit of old objects",
+      oldmalloc_increase_bytes: "Total bytes allocated to old objects",
+      oldmalloc_increase_bytes_limit: "Bytes limit that will trigger garbage collection of old objects",
+
+      # Ruby 3.0
+      time: "The total time spent in garbage collections",
+      read_barrier_faults: "The total number of times the read barrier was triggered during compaction",
+      total_moved_objects: "The total number of objects compaction has moved",
+
+      # Ruby 3.3
+      marking_time: "Time spent in the marking phase",
+      sweeping_time: "Time spent in the sweeping phase"
+    }.freeze
+
     Yabeda.configure do
       group :gc
 
-      gauge :count, tags: [], comment: "Count of all GCs"
-      gauge :compact_count, tags: [], comment: "Count of all GC compactions"
-      gauge :minor_gc_count, tags: [], comment: "Count of minor GCs"
-      gauge :major_gc_count, tags: [], comment: "Count of major GCs"
-      gauge :heap_allocated_pages, tags: [], comment: "Total number of pages allocated for the heap"
-      gauge :heap_sorted_length, tags: [], comment: "Length of the sorted heap"
-      gauge :heap_allocatable_pages, tags: [], comment: "Total number of allocatable heap pages"
-      gauge :heap_available_slots, tags: [], comment: "Total number of slots in heap pages"
-      gauge :heap_live_slots, tags: [], comment: "Number of live objects slots"
-      gauge :heap_free_slots, tags: [], comment: "Number of free object slots"
-      gauge :heap_final_slots, tags: [], comment: "Number of object slots with finalizers attached to them"
-      gauge :heap_marked_slots, tags: [],
-                                comment: "Count of old objects which survived more than 3 GC cycles and number of write-barrier unprotected objects"
-      gauge :heap_eden_pages, tags: [], comment: "Number of pages allocated for the eden heap"
-      gauge :heap_tomb_pages, tags: [], comment: "Number of pages allocated for the tomb heap"
-      gauge :total_allocated_pages, tags: [],
-                                    comment: "Total number of allocated pages over the lifetime of this process"
-      gauge :total_freed_pages, tags: [], comment: "Total number of freed pages over the lifetime of this process"
-      gauge :total_allocated_objects, tags: [],
-                                      comment: "Total number of allocated objects over the lifetime of this process"
-      gauge :total_freed_objects, tags: [], comment: "Total number of freed objects over the lifetime of this process"
-      gauge :malloc_increase_bytes, tags: [], comment: "Total bytes allocated to objects"
-      gauge :malloc_increase_bytes_limit, tags: [],
-                                          comment: "Bytes limit that will trigger garbage collection of objects"
-      gauge :remembered_wb_unprotected_objects, tags: [],
-                                                comment: "Number of write-barrier unprotected objects in the remembered set"
-      gauge :remembered_wb_unprotected_objects_limit, tags: [],
-                                                      comment: "Limit on write-barrier unprotected objects allowed in the remembered set"
-      gauge :old_objects, tags: [], comment: "Number of old objects"
-      gauge :old_objects_limit, tags: [], comment: "Limit of old objects"
-      gauge :oldmalloc_increase_bytes, tags: [], comment: "Total bytes allocated to old objects"
-      gauge :oldmalloc_increase_bytes_limit, tags: [],
-                                             comment: "Bytes limit that will trigger garbage collection of old objects"
-
-      if RUBY_VERSION >= "3.0"
-        gauge :time, tags: [], comment: "The total time spent in garbage collections"
-        gauge :read_barrier_faults, tags: [], comment: "The total number of times the read barrier was triggered during compaction"
-        gauge :total_moved_objects, tags: [], comment: "The total number of objects compaction has moved"
-      end
-
-      gauge :time, tags: [], comment: "The total time spent in garbage collections" if RUBY_VERSION >= "3.1"
-
-      if RUBY_VERSION >= "3.3"
-        gauge :marking_time, tags: [], comment: "Time spent in the marking phase"
-        gauge :sweeping_time, tags: [], comment: "Time spent in the sweeping phase"
+      # Register gauges for all GC stats. Include our optional commentary.
+      ::GC.stat.each_key do |stat_name|
+        gauge stat_name, tags: [], comment: comments.fetch(stat_name, stat_name)
       end
 
       collect do
